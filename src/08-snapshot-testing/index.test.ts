1e9926ad6c5fbdbfbc179c0f981b6e2e4a1665e3
Val-d-emar <vhot@live.ru>
2025-11-06 23:24:25 +0400

    fix: 08

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -5,17 +5,32 @@ describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
     // Write your test here
-    const tree = generateLinkedList(['http://ya.ru']);
-    expect(tree).toMatchSnapshot();
+    const values = [1, 2, 3];
+    const tree = generateLinkedList(values);
+
+    const expectedList = {
+      value: 1,
+      next: {
+        value: 2,
+        next: {
+          value: 3,
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    };
+
+    expect(tree).toStrictEqual(expectedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
     // Write your test here
-    const tree = generateLinkedList([
-      'http://ya.ru',
-      'https://jsonplaceholder.typicode.com',
-    ]);
+    const values = ['a', 'b', 'c'];
+    const tree = generateLinkedList(values);
+
     expect(tree).toMatchSnapshot();
   });
 });
